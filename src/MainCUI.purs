2f59d79d2b9f8161db40403d4cb783870ce1e677
bombrary <bombra108@gmail.com>
2021-07-09 19:29:55 +0900

    cp Main.purs -> MainCUI.purs

---
+++ src/MainCUI.purs
@@ -0,0 +1,28 @@
+module MainCUI where
+
+import Prelude
+
+import Brainfuck (run) as B
+import Brainfuck.CUI (cuiLog, cuiStream)
+import Brainfuck.CUI.State (init) as CUIState
+import Brainfuck.Program (fromString, Program) as BP
+import Effect (Effect)
+import Effect.Aff (Aff, launchAff_)
+import Effect.Class (liftEffect)
+import Node.ReadLine (createConsoleInterface, noCompletion, close) as RL
+import Node.ReadLine.Aff (question) as RL
+
+main :: Effect Unit
+main = do
+  ref <- CUIState.init
+  launchAff_ do
+    program <- inputProgram
+    B.run (cuiStream ref) (cuiLog ref) program
+
+
+inputProgram :: Aff BP.Program
+inputProgram = do
+  interface <- liftEffect $ RL.createConsoleInterface RL.noCompletion
+  s <- RL.question "program> " interface
+  liftEffect $ RL.close interface
+  pure (BP.fromString s)

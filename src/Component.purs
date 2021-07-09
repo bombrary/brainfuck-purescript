dd725b5d01ef1170d9178b5b69664eaa502176fb
bombrary <bombra108@gmail.com>
2021-07-09 19:25:39 +0900

    implemented viz

--- src/Component.purs
+++ src/Component.purs
@@ -13,17 +13,20 @@ import Brainfuck.Interp (InterpResult) as BI
 import Brainfuck.Program (fromString) as BP
 import Data.Maybe (Maybe(..))
 import Data.Either (Either(..))
+import Effect.Aff.AVar (AVar, put, take) as AVar
 import Effect.Aff.Class (class MonadAff, liftAff)
+import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
-import Data.String.CodeUnits (singleton) as CodeUnits
+import Data.String.CodeUnits (singleton, toChar, take) as CodeUnits
 import Data.Traversable (for_)
-import Effect.Aff (Aff)
 import Halogen.Subscription as HS
 
-import Effect.Aff.AVar (AVar, put, take) as AVar
-import Data.String.CodeUnits (toChar, take) as CodeUnits
+import Brainfuck.Interp.Log (Log(..)) as BIL
+import Brainfuck.Program (Program(..)) as BP
+import Data.Array (mapWithIndex) as Array
+import Effect.Aff (delay, Milliseconds(..))
 
 
 data Action
@@ -34,6 +37,7 @@ data Action
   | RequestInput
   | ChangeInput String
   | ConfirmInput
+  | RequestLogState BS.State
 
 
 type State =
@@ -45,6 +49,8 @@ type State =
   , avar :: AVar.AVar Char
   , input :: String
   , isInputEnabled :: Boolean
+  , logMay :: Maybe (BIL.Log Aff)
+  , stateMay :: Maybe (BS.State)
   }
 
 
@@ -58,6 +64,8 @@ initialState avar =
   , avar
   , input: ""
   , isInputEnabled: false
+  , logMay: Nothing
+  , stateMay: Nothing
   }
 
 
@@ -81,6 +89,12 @@ render state =
         then inputArea
         else HH.div_ []
     , outputArea state.output
+    , case state.stateMay of
+        Just s ->
+          stateArea (BP.fromString state.program) s
+
+        Nothing ->
+          HH.div_ []
     , case state.result of
         Just res ->
           interpResult res
@@ -88,6 +102,37 @@ render state =
         Nothing ->
           HH.div_ []
     ]
+
+stateArea :: forall w i. BP.Program -> BS.State -> HH.HTML w i
+stateArea program (BS.State { iptr, dptr, memory }) =
+  HH.div_
+    [ programLogArea iptr program
+    , memoryLogArea dptr memory
+    ]
+
+
+mapWithASpecialIndex :: forall a b. Int -> (a -> b) -> (a -> b) -> Array a -> Array b
+mapWithASpecialIndex j fThen fElse =
+  Array.mapWithIndex (\i x -> if i == j then fThen x else fElse x)
+
+
+programLogArea :: forall w i. Int -> BP.Program -> HH.HTML w i
+programLogArea iptr (BP.Program program) =
+  HH.div_ $
+    mapWithASpecialIndex iptr
+      (\cmd -> HH.span [ HP.style "background-color: salmon;" ] [ HH.text $ show cmd ])
+      (\cmd -> HH.span [ HP.style "background-color: white;" ] [ HH.text $ show cmd ])
+      program
+
+
+memoryLogArea :: forall w i. Int -> Array Int -> HH.HTML w i
+memoryLogArea dptr memory =
+  HH.div_ $
+    mapWithASpecialIndex dptr
+      (\dat -> HH.span [ HP.style "background-color: salmon; margin-right: 1em;" ] [ HH.text $ show dat ])
+      (\dat -> HH.span [ HP.style "background-color: white; margin-right: 1em;" ] [ HH.text $ show dat ])
+      memory
+
 
 inputArea :: forall w. HH.HTML w Action
 inputArea =
@@ -148,16 +193,20 @@ handleAction =
     Initialize -> do
       { emitter, listener } <- liftEffect HS.create
       _ <- H.subscribe emitter
-      H.modify_ \s -> s { streamMay = Just $ createStream listener s.avar }
+      H.modify_ \s ->
+        s { streamMay = Just $ createStream listener s.avar
+          , logMay = Just $ createLog listener
+          }
 
     ChangeProgram program ->
       H.modify_ _ { program = program }
 
     ExecuteProgram -> do
-      { program, streamMay } <- H.get
-      for_ streamMay \s -> do
+      { program, streamMay, logMay } <- H.get
+      for_ streamMay \s ->
+        for_ logMay \l -> do
           H.modify_ _ { output = "" , result = Nothing, isExecutable = false }
-          res <- liftAff $ B.run s BIL.noLog (BP.fromString program)
+          res <- liftAff $ B.run s l (BP.fromString program)
           H.modify_ _ { result = Just res, isExecutable = true }
 
     Output c ->
@@ -179,6 +228,9 @@ handleAction =
         Nothing ->
           pure unit
 
+    RequestLogState state -> do
+       H.modify_ _ { stateMay = Just state }
+
 
 createStream :: HS.Listener Action -> AVar.AVar Char -> BIS.Stream Aff
 createStream listener avar = BIS.Stream { input, output }
@@ -189,3 +241,17 @@ createStream listener avar = BIS.Stream { input, output }
 
     output c =
       liftEffect $ HS.notify listener (Output c)
+
+
+createLog :: HS.Listener Action -> BIL.Log Aff
+createLog listener = BIL.Log
+    { onStart: pure unit
+    , onState
+    , onCmd: \_ -> pure unit
+    , onEnd: pure unit
+    }
+  where
+    onState state = do
+      liftEffect $ HS.notify listener (RequestLogState state)
+      liftAff $ delay (Milliseconds 100.0)
+
